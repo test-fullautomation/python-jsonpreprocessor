bbab8c8a1457b903dc891282c44ea70fbdc0bfe1
Thomas Pollerspöck <Thomas.Pollerspoeck@de.bosch.com>
2022-03-24 17:28:53 +0100

    extend samples

---
+++ sample/003_sample_import.py
@@ -0,0 +1,37 @@
+#  Copyright 2020-2022 Robert Bosch Car Multimedia GmbH
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#################################################################################
+
+## This example also shows how to import json files
+## into json files.
+
+import sys
+
+sys.path.append('D:\B\python-jsonpreprocessor')
+
+from JsonPreprocessor import CJsonPreprocessor
+
+from pprint import pprint
+
+prepro=CJsonPreprocessor()
+
+# you can load the base json file with
+# - relative path to your python program
+# - absolute path
+# - paths containting environment variables by means of
+#   %envvariable% syntax
+data=prepro.jsonLoad(".\json\json_with_import.json")
+
+pprint(data)
+
