21cd0380bb829959b3b7f02291116766c3e42f79
mas2hc <son.maidinhnam@vn.bosch.com>
2025-05-13 14:22:40 +0700

    Ticket 408 - Blanks around key names

--- JsonPreprocessor/CJsonPreprocessor.py
+++ JsonPreprocessor/CJsonPreprocessor.py
@@ -1980,6 +1980,7 @@ This function handle a last element of a list or dictionary
                 self.__reset()
                 raise Exception(f"{error} in line: '{line}'")
             if "${" in line:
+                line = regex.sub(r'\${\s*([^\s][^}]+[^\s])\s*}', '${\\1}', line)
                 curLine = line
                 tmpList03 = []
                 while regex.search(r'\${([^}]*)}', line):
